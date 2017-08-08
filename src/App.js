7e1a84dc0c1ef34ed5a8578b94d9c9b6740b2497
Brandon M <development@brandonmcginnis.com>
2017-08-07 20:47:11 -0400

    Added styled-components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-class App extends Component {
+
+/*class App extends Component {
   render() {
     return (
       <div className="App">
@@ -16,6 +18,79 @@ class App extends Component {
       </div>
     );
   }
-}
+}*/
+
+const Home = () => (
+  <div>
+    <h2>H</h2>
+  </div>
+);
+
+const Calculator = () => (
+  <div>
+    <h2>Calculator</h2>
+  </div>
+);
+
+const Converter = () => (
+  <div>
+    <h2>Conversions</h2>
+  </div>
+);
+
+/*
+const Topic = ({ match }) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+);
+
+
+const Topics = ({ match }) => (
+  <div>
+    <h2>Topics</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>
+          Rendering with React
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/components`}>
+          Components
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>
+          Props v. State
+        </Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.url}/:topicId`} component={Topic}/>
+    <Route exact path={match.url} render={() => (
+      <h3>Please select a topic.</h3>
+    )}/>
+  </div>
+);
+*/
+
+const App = () => (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/calculator">Calculator</Link></li>
+        <li><Link to="/converter">Converter</Link></li>
+      </ul>
+
+      <hr/>
+
+      <Route exact path="/" component={Home}/>
+      <Route path="/calculator" component={Calculator}/>
+      <Route path="/converter" component={Converter}/>
+    </div>
+  </Router>
+);
 
 export default App;
